041b46da28e2732fdf3f72ab86e01d496f8bc00c
anamshk <89604126+anamshk@users.noreply.github.com>
2021-11-19 18:42:29 -0500

    updates with @media

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,16 +10,16 @@ const createTweetElement = (data) => {
       <span class="userprof"><img style="width: 50%" src ="${data.user.avatars}"/></span>
       <span class="username">${data.user.name}</span>
     </div>
-    <p class="userhandle">${data.user.handle}</p>
+    <p class="userhandle"><strong>${data.user.handle}</strong></p>
   </div>
   <form> 
     <label id="body"><strong>${escape(data.content.text)}</strong></label> 
   </form>
   <footer class="bottom-bar">
     <div id='date'>
-      <p class='tweet-time'>${timeago.format(data.created_at)}</p>
+      <p class='tweet-time'><strong>${timeago.format(data.created_at)}</strong></p>
     </div>
-    <p class='icons'>
+    <p class='icons' class=overlimit>
       <i class="fab fa-font-awesome-flag"></i>
       <i class="fas fa-retweet"></i>
       <i class="far fa-heart"></i>
@@ -29,28 +29,28 @@ const createTweetElement = (data) => {
   return $markup;
 };
 
+//function to help prevent XSS with Escaping and keep the content safe
 const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-const loadTweets = function () {
+const loadTweets = function() {
   let url = 'http://localhost:8080/tweets';
   $.ajax({
     url: url,
-    method: "GET",
-    // data: $(this).serialize()
+    method: "GET"
   })
     .then((result)=>{
       // console.log('result:',result);
       renderTweets(result);
     });
-  return;
+  return $('.tweetfeed').empty();
 };
 
 const renderTweets = function(data) {
-  $('.tweetfeed').empty();
+  // $('.tweetfeed').empty();
   data.forEach(tweet => {
     const $tweet = createTweetElement(tweet);
     $('.tweetfeed').prepend($tweet);
@@ -59,13 +59,13 @@ const renderTweets = function(data) {
   
 //POST req with AJAX
 $(document).ready(() => {
-  // loadTweets();
   //if statements to validate form before posting
   const $form = $('.form');
   $($form).submit(function(event) {
     event.preventDefault();
+    //giving the appropriate error message with slidedown
     if ($(".counter").val() < 0) {
-      $(".alert").text('Your tweet has excedeed the maximum length.').show();
+      $(".alert").text('Your tweet has exceeded the maximum length.').show();
     } else if ($(".counter").val() > 139) {
       $(".alert").text('Your tweet has no text.').show();
     } else {
@@ -81,6 +81,9 @@ $(document).ready(() => {
           loadTweets();
         });
     }
+// help to clear text from the textarea once you click tweet
+$("textarea").val('');
+$("output").val(140);
   });
 });
 
